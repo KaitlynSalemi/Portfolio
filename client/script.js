ac61cfa99048aa452455fbb367556b588e71a630
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-11-25 15:42:43 -0600

    small UX changes

--- client/script.js
+++ client/script.js
@@ -1,48 +1,48 @@
-// document.querySelector(".navigation__heading").innerHTML = "Java Script file is working.";
-const form = document.querySelector(".section-contact__form");
-const nameInput = document.querySelector(".name-input");
-const emailInput = document.querySelector(".email-input");
-const messageInput = document.querySelector(".message-textarea");
-const sent = document.querySelector(".section-contact__form__sent");
+// document.querySelector(".navigation__heading").innerHTML = "JavaScript file is working.";
+const form = document.querySelector('.section-contact__form');
+const nameInput = document.querySelector('.name-input');
+const emailInput = document.querySelector('.email-input');
+const messageInput = document.querySelector('.message-textarea');
+const sent = document.querySelector('.section-contact__form__sent');
 
 function sentMessage(message) {
-    //change the innerHtml of the sent element
-    sent.innerHTML = message
+  //change the innerHtml of the sent element
+  sent.innerHTML = message;
 }
 
 function checkInputs() {
-    const name = nameInput.value.trim();
-    const email = emailInput.value.trim();
-    const message = messageInput.value.trim();
+  const name = nameInput.value.trim();
+  const email = emailInput.value.trim();
+  const message = messageInput.value.trim();
 
-    if(name === '') {
-        //change sent message
-        sentMessage("Name input cannot be blank.");
-    } else if(email === '') {
-        sentMessage("Email input cannot be blank.");
-    } else if(message === '') {
-        sentMessage("Message input cannot be blank.");
-    } else {
-        //the message has been sent
-        sentMessage("Your message has successfully sent!"); 
-        form.reset();
-    }
+  if (name === '') {
+    //change sent message
+    sentMessage('Name input cannot be blank.');
+  } else if (email === '') {
+    sentMessage('Email input cannot be blank.');
+  } else if (message === '') {
+    sentMessage('Message input cannot be blank.');
+  } else {
+    //the message has been sent
+    sentMessage('Your message has successfully sent!');
+    form.reset();
+  }
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', e => {
+  e.preventDefault();
 
-    const name = nameInput.value.trim();
-    const email = emailInput.value.trim();
-    const message = messageInput.value.trim();
+  const name = nameInput.value.trim();
+  const email = emailInput.value.trim();
+  const message = messageInput.value.trim();
 
-    const formData = {
-        name,
-        email,
-        message
-    }
+  const formData = {
+    name,
+    email,
+    message,
+  };
 
-    $.post('/email', formData)
+  $.post('/email', formData);
 
-    checkInputs();
+  checkInputs();
 });
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const sendMail = require('./mail');
 const app = express();
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: false }));
@@ -13,20 +13,20 @@ app.use(express.static(__dirname + '/client'));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/index.html'));
-})
+  res.sendFile(path.join(__dirname, '/client/index.html'));
+});
 
 app.post('/email', (req, res) => {
-    const { name, email, message} = req.body;
+  const { name, email, message } = req.body;
 
-    sendMail(name, email, message, function(err, data) {
-        if(err) {
-            res.status(500).json({ message: 'Internal Error' })
-        } else {
-            res.json({ message: 'Email sent!!'})
-        }
-    })
-})
+  sendMail(name, email, message, function (err, data) {
+    if (err) {
+      res.status(500).json({ message: 'Internal Error' });
+    } else {
+      res.json({ message: 'Email sent!!' });
+    }
+  });
+});
 
 // Listening on the Port
-app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
